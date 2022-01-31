4fa116858e5531b32ca3aeedcf38a28e255eab12
Your Name <sofabb9@gmail.com>
2022-02-01 01:12:09 +0700

    add

--- src/App.js
+++ src/App.js
@@ -2,15 +2,27 @@ import './App.css';
 import Header from './components/Header/Header.jsx';
 import ProfileContent from './components/Content/Profile/ProfileContent.jsx';
 import NavBar from './components/Navbar/NavBar.jsx';
+import Dialogs from './components/Content/Dialogs/Dialogs';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 const App = () => {
   return (
-    <div className="App">
-      <Header />
-      <NavBar />
-      {/* <ProfileContent /> */}
-      <Dialogs />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <NavBar />
+        <div class="app-wrapper-content">
+          <Routes> {/* Именно ROUTES */}
+            <Route path="/dialogs" element={<Dialogs />} /> {/*Помним, что надо обрамить 
+        все <BrowserRouter></BrowserRouter> */}
+
+            <Route path="/profile" element={<ProfileContent />} />
+            {/* <ProfileContent /> */}
+            {/* <Dialogs /> */}
+          </Routes>
+        </div>
+      </div >
+    </BrowserRouter>
   );
 }
 
--- src/components/Navbar/NavBar.jsx
+++ src/components/Navbar/NavBar.jsx
@@ -6,15 +6,16 @@ const NavBar = () => {
         <div className="NavBar">
             <div className="MainNavBar">
                 <ul className="item">
-                    <li><a href='#s'>Profile</a></li>
-                    <li><a href='#s'>News</a></li>
-                    <li><a href='#s'>Dialogs</a></li>
-                    <li><a href='#s'>Music</a></li>
+                    <li><a href='/profile'>Profile</a></li>
+                    <li><a href='/news'>News</a></li>
+                    <li><a href='/dialogs'>Dialogs</a></li>
+                    <li><a href='/friends'>Friends</a></li>
+                    <li><a href='/music'>Music</a></li>
                 </ul>
             </div>
             <div className="Add-NavBar">
                 <ul className="item">
-                    <li><a href='#s'>Settings</a></li>
+                    <li><a href='/setting'>Settings</a></li>
                 </ul>
             </div>
         </div>
